4be14d210a2be0695a4e84db2116cee9a89b2183
Mateusz <kaspazza@gmail.com>
2018-09-05 18:32:27 +0200

    added fields to xmls and created saving to files methods

--- ccms/src/main/java/connectors/Connector.java
+++ ccms/src/main/java/connectors/Connector.java
@@ -3,57 +3,117 @@ package connectors;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-
+import java.lang.Exception;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import java.util.HashMap;
 
 public class Connector {
 
     Model model;
 
-    public void addAssignmentToXML(String fileName, String assignmentName){
+    public void addAssignmentToXML(String assignmentName){
+
         try{
 
-            File xmlFile = new File("/java/XMLs/" fileName + ".xml");
+            File xmlFile = new File("/java/XMLs/assignments.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(fXmlFile);
-            doc.getDocumentElement().normalize();
+            doc.getDocumentElement().normalize();      
 
-            String tag = fileName.substring(0, str.length() - 1)            
+            Element root = doc.getDocumentElement(); // employees
+            Element assignmentTag = (Element) root.getElementsByTagName("assignments").item(0); //employee
+            Element newAssignment = doc.createElement("assignment").setAttribute("name", assignmentName);
 
-            // Element root = doc.getDocumentElement(); // employees
+            assignmentTag.appendChild(newAssignment);
 
-            // Element TagName =  (Element) dataTag.getElementsByTagName(tag).item(0); // employee
-
-            Element newPerson = doc.createElement(tag);
-
-            Element firstName = doc.createElement("firstName");
-            firstName.setTextContent("Tom");
-
-            Element lastName = doc.createElement("lastName");
-            lastName.setTextContent("Hanks");
-
-            newPerson.appendChild(firstName);
-            newPerson.appendChild(lastName);
-
-            peopleTag.appendChild(newPerson);
-
+            try{
+                String outputURL = "/java/XMLs/assignments.xml";
+                
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new FileOutputStream(outputURL));
+                
+                TransformerFactory transFactory = TransformerFactory.newInstance();
+                Transformer transformer = transFactory.newTransformer();
+                
+                transformer.transform(source, result);
+                
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
         } catch (Exception e) {
-            view.showMessage(e);
+            e.printStackTrace();
         }
     }
 
-    public void saveXML(Model model) {
+    public void addPersonToXML(Model model){
+        
+        String accountType = model.get(accountType).toLowerCase();
+        String name = model.get(name).toLowerCase();
+        String capitalizedName = name.substring(0, 1).toUpperCase() + name.substring(1);
+        String surname = model.get(surname).toLowerCase();
+        String capitalizedSurname = surname.substring(0, 1).toUpperCase() + surname.substring(1);
+        String login = model.get(login);
+        String password = model.get(password);
+
+        if(accountType.equals("students")){
+            HashMap<String, Assignment> assignments = model.get(assignments);
+        }
+
+
+        File xmlFile = new File("/java/XMLs/" + personType +".xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
+        doc.getDocumentElement().normalize();
+
+        String tag = personType.substring(0, str.length() - 1);
+
+        Element root = doc.getDocumentElement(); // employees
+
+        Element personTag = (Element) root.getElementsByTagName(accountType).item(0); //employee
+
+        Element newPerson = doc.createElement(tag).setAttribute("login", login);
+
+        Element userName = doc.createElement("name");
+        userName.setTextContent(capitalizedName);
+
+        Element userSurname = doc.createElement("surname");
+        userSurname.setTextContent(capitalizedSurname);
+
+        Element userPassword = doc.createElement("password");
+        userPassword.setTextContent(password);
+
+        newPerson.appendChild(userName);
+        newPerson.appendChild(userSurname);
+        newPerson.appendChild(userPassword);
+        
+        personTag.appendChild(newPerson);
+        
+        try{
+            String outputURL = "/java/XMLs/" + personType +".xml";
+            
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new FileOutputStream(outputURL));
+            
+            TransformerFactory transFactory = TransformerFactory.newInstance();
+            Transformer transformer = transFactory.newTransformer();
+            
+            transformer.transform(source, result);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Element loadPerson(String login){
 
     }
 
-    public void loadXML(String name, String surname, String password){
-
-    }
-
-    public void loadXML(String name, String surname){
+    public Element loadAssigments(){
 
     }
 }
