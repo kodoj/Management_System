715123d72e50f945c980358606b84f8cdd83078e
Karol Trzaska <karolantonitrzaska@gmail.com>
2018-09-05 14:23:49 +0200

    DAO interface made

--- ccms/src/main/java/DAO/DAO.java
+++ ccms/src/main/java/DAO/DAO.java
@@ -1,4 +1,10 @@
 package DAO;
 
+import containers.Model;
+
 public interface DAO {
+    public void getData();
+    public void setData();
+    public void deleteData();
+    public Model createModel();
 }
