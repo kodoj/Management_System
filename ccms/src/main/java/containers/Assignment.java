a6493d82b1ca8adb44e6c382d55d240a93d83e64
Karol Trzaska <karolantonitrzaska@gmail.com>
2018-09-05 10:59:15 +0200

    assignment done, container for informations about assignments

--- ccms/src/main/java/containers/Assignment.java
+++ ccms/src/main/java/containers/Assignment.java
@@ -1,6 +1,49 @@
-package DAO;
+package containers;
 
 public class Assignment {
+    private String id;
+    private String url;
+    private Integer grade;
+    private Boolean isFinished;
 
-    
+
+        Assignment(String id){
+            this.id = id;
+        }
+
+
+        public void setUrl(String url){
+            this.url = url;
+        }
+
+
+        public void setGrade(Integer grade){
+            this.grade = grade;
+        }
+
+
+        public void setIsFinished(Boolean isFinished){
+            this.isFinished = isFinished;
+        }
+
+
+        public String getId(){
+            return this.id;
+        }
+
+
+        public String getUrl(){
+            return this.url;
+        }
+
+
+        public Integer getGrade(){
+            return this.grade;
+        }
+
+
+        public Boolean getIsFinished(){
+            return this.isFinished;
+        }
+
 }
